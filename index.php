5e877153a00e468354fa7b7d63d08df2bc9a5fca
SGS Sandhu <sgs.sandhu@gmail.com>
2018-09-13 15:39:48 +0530

    Update

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 	<!-- Include our stylesheet -->
 	<link href="assets/css/styles.css" rel="stylesheet"/>
-	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="assets/js/script.js"></script>
 </head>
 <body>
